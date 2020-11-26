4167c12e9413de22515c0f3ab08ccc7c412baea5
sam <sblan45@gmail.com>
2020-11-25 23:32:40 -0500

    adds styling to seasonDisplay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,23 +1,34 @@
 import React from "react"
+import './SeasonDisplay.css'
+
+const seasonConfig = {
+  summer: {
+    text: "lets go to the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "brrr its FREEZING outside",
+    iconName: "snowflake"
+  }
+}
 
 function getSeason(lat, month) {
   if (month > 2 && month < 9) {
-    return lat > 0 ? 'Summer' : 'Winter'
+    return lat > 0 ? 'summer' : 'winter'
   } else {
-    return lat > 0 ? 'Winter' : 'Summer'
+    return lat > 0 ? 'winter' : 'summer'
   }
 }
 
 function SeassonDisplay(props) {
   const season = getSeason(props.lat, new Date().getMonth())
-  console.log(season)
+  const {text, iconName} = seasonConfig[season]
+
   return (
-    <div>
-      Season: {season}
-      <br></br>
-      Lat: {props.lat}
-      <br></br>
-      Month: {new Date().getMonth()}
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   )
 }
